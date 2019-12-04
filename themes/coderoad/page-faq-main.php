dbfd04a49f28caa2c3ff6a0e550e22d924dea2d6
percevil <peeevil@gmail.com>
2019-12-04 13:14:10 -0800

    faq taxonomy setup

--- themes/coderoad/page-faq-main.php
+++ themes/coderoad/page-faq-main.php
@@ -45,18 +45,6 @@ get_header();?>
         </div>
 
     </section>
-    
 
-
-
-	<!-- <div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
-				'after'  => '</div>',
-			) );
-		?> -->
-	</div><!-- .entry-content -->
 </div>
 <?php get_footer(); ?>
