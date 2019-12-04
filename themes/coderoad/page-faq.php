1c8e248d35240281a0a830cd8235a57767a6f800
percevil <peeevil@gmail.com>
2019-12-04 11:04:56 -0800

    save

--- themes/coderoad/page-faq.php
+++ themes/coderoad/page-faq.php
@@ -25,8 +25,8 @@ get_header();?>
         <div class="tutorial-h2">
             <h2 >Tutorials/ Creating</h2>
             <div class="tutorial-accent"></div>
-          
         </div>
+
         <div class="link-container">
             <a href="">Where an I see my progress?</a>
             <a href="">How do I create a tutorial?</a>
@@ -37,6 +37,7 @@ get_header();?>
     <section class="profile-container">
         <div class="profile-h2">
             <h2>Profile/ Account</h2>
+            <div class="profile-accent"></div>
         </div>
         <div class="link-container">
             <a href="">How do I change my profile picture?</a>
