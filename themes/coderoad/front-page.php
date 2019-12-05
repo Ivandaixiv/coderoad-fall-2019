66edd08246ea9b444e4e97174ba65e30124c1791
Maja <rojasmaja5@gmail.com>
2019-12-05 13:22:16 -0800

    started the front page with deliting the default page

---
+++ themes/coderoad/front-page.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The main template file.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+<div class="site-image">
+
+<?php the_post_thumbnail(   'feature-post image'    ); ?>
+
+
+
+</div>
+
+		
+
+	
+
+			
+	
+
+
+
+
+<?php get_footer(); ?>
