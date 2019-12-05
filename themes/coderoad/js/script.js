0872e676fa9dec892c84a841936fc220a04e8fff
Ivan <ivandaixivwork@gmail.com>
2019-12-04 16:41:51 -0800

    Script merge conflict

--- themes/coderoad/js/script.js
+++ themes/coderoad/js/script.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-(function($) {
-  $(function() {
-    const searchBtn = $('#search-icon');
-    const searchField = $('#search-field');
-    searchBtn.on('click', function() {
-      searchField.css('width', '100vw');
-      searchBtn.css('position', 'absolute');
-=======
 (function($){
     $(function(){
         const searchBtn = $("#search-icon");
@@ -14,8 +5,6 @@
         searchBtn.on('click', function(){
             searchField.css({'position':'absolute', 'width': '80vw','right': '0','margin':'20px'});
             searchBtn.css({'position':'absolute','left' : '0 '});
-        })
->>>>>>> baecbe154b06ad77074eefb2c20f55e1784ba603
     });
   });
 })(jQuery);
