d7eb5e10423201a2d31d4f67c9cdb395394d01f8
Ivan <ivandaixivwork@gmail.com>
2019-12-05 20:55:44 -0800

    Hamburger Dropmenu backend completed and fixed issue with conflicting javascript

--- themes/coderoad/js/script.js
+++ themes/coderoad/js/script.js
@@ -4,15 +4,36 @@
         const searchField = $("#search-field");
         const page = $("#content");
         const cancelBtn = $('#cancel-icon');
-        let isOpened = false;
+        const dropMenuBtn = $('#hamburger-icon');
+        const burgerMenu = $("#site-navigation ul");
+        const footerSection = $('#colophon')
 
+        let isOpened = false;
+        let isDropped = false;
+
+        function dropMenu(){
+            isDropped = true;
+            burgerMenu.css({'display': 'flex'});
+        }
+        function undropMenu() {
+            isDropped = false;
+            burgerMenu.css({'display': 'none'});
+        }
+        function darkenBack(){
+            page.css({'opacity':'50%','filter': 'brightness(20%)'});
+            footerSection.css({'opacity':'50%','filter': 'brightness(20%)'});
+        }
+        function brightenBack() {
+            page.css({'opacity':'1','filter': 'brightness(1)'});
+            footerSection.css({'opacity':'1','filter': 'brightness(1)'});
+        }
         function openField(){
             isOpened = true;
             searchField.css({'position':'absolute', 'width': '70vw','right': '15%',});
             searchBtn.css({'transform': 'translate(-73vw,0)'});
             cancelBtn.css({'display':'flex'})
             setTimeout(function(){
-                cancelBtn.css({'transform': 'translate(-15vw,0)'});
+                cancelBtn.css({'transform': 'translate(-20vw,0)'});
             }, 200);
             setTimeout(function(){
                 searchField.css({'border':'1px solid black'});
@@ -26,15 +47,25 @@
             searchBtn.css({'transform': 'translate(0,0)'});
             cancelBtn.css({'transform': 'translate(10vw,0)'});
             setTimeout(function(){
-                cancelBtn.css({'display':'none'})
+                cancelBtn.css({'display':'none'});
             }, 1500);
         }
 
+        dropMenuBtn.on('click',function(){
+            if(isDropped){
+                undropMenu();
+            }
+            else{
+                closeField();
+                dropMenu();
+            }
+        });
         searchBtn.on('click', function(){
             if(isOpened) {
                 closeField();
             }
             else {
+                undropMenu();
                 openField();
             }
         });
@@ -43,6 +74,15 @@
         });
         page.on('click',function(){
             closeField();
+            if(isDropped && isOpened == false){
+                undropMenu();
+            }
+            else {
+                closeField();
+                setTimeout(function(){
+                    undropMenu();
+                })
+            }
         });
   });
 })(jQuery);
