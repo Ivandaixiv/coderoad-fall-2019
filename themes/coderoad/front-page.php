2dcb7788ac1f8da1abe5d9f27217c03316438e4a
Maja <rojasmaja5@gmail.com>
2019-12-09 16:25:08 -0800

    added section

--- themes/coderoad/front-page.php
+++ themes/coderoad/front-page.php
@@ -58,6 +58,26 @@ get_header(); ?>
  <img  src="<?php echo home_url(); ?>/wp-content/uploads/2019/12/Mail@3x.png">
 </div>
 
+<div class="subject">
+<?php if(get_field('subject')): ?>
+    <?php while(the_repeater_field('subject')): ?>
+        <div class="all-sub">
+        <img src="<?php echo the_sub_field('image'); ?>" >
+        <div class="sub-subject">
+        <h3><?php the_sub_field('title'); ?></h3>
+        <p><?php the_sub_field('date'); ?></p>
+        <p><?php the_sub_field('instructor'); ?></p>
+       <img src="<?php echo the_sub_field('rating'); ?>" >
+    </div>
+    </div>
+    <?php endwhile; ?>
+ <?php endif; ?>
+
+
+
+
+    </div>
+
 
 
 
