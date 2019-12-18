195b6c70a29b22c300d4bfe5324fae6a9fcb4418
Maja <rojasmaja5@gmail.com>
2019-12-17 21:48:00 -0800

    styled search page

--- themes/coderoad/search.php
+++ themes/coderoad/search.php
@@ -9,13 +9,14 @@ get_header(); ?>
 
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+			
+		<div class="search-content">
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
 				<h1 class="page-title"><?php printf( esc_html( 'Search Results for: %s' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 			</header><!-- .page-header -->
-
+			
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
@@ -30,6 +31,7 @@ get_header(); ?>
 			<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
 		<?php endif; ?>
+		</div>
 
 		</main><!-- #main -->
 	</section><!-- #primary -->
